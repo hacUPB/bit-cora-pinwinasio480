c1f46de60260b3f9d24b1ce60d3bf17b6aca18fb
Samuel Gomez <samuel.gomezepb.edu.co>
2025-07-22 09:26:45 -0500

    Ejercicios reto primeros

---
+++ Unidad 1 Actividad 1/Reto/Reto1a7.asm
@@ -0,0 +1,26 @@
+//RETO PUNTOS 1 AL 7
+
+//1. Carga en D el valor 1978.
+
+@1978
+D=A
+
+//2. Guarda en la posición 100 de la RAM el número 69.
+
+@69
+D=A
+@100
+M=D
+
+//3. Guarda en la posición 200 de la RAM el contenido de la posición 24 de la RAM.
+
+@97
+D=A
+@24
+M=D
+@200
+M=D
+
+//4. Lee lo que hay en la posición 100 de la RAM, resta 15 y guarda el resultado en la posición 100 de la RAM.
+
+//
