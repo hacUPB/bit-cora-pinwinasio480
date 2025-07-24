20f4eb329b669b2621e4e1b44dfbc652e7fbf19a
Samuel Gomez Espitia <samuel.gomeze@upb.edu.co>
2025-07-23 23:20:38 -0500

    update

--- Unidad 1 Actividad 1/Reto/TesteoCodigosRetos.asm
+++ Unidad 1 Actividad 1/Reto/TesteoCodigosRetos.asm
@@ -1,18 +1,13 @@
-    // i = 1000
-    @1000
-    D=A
-    @i
-    M=D
-    (LOOP)
-    // if (i == 0) goto CONT
-    @i
-    D=M
-    @CONT
-    D;JEQ
-    // i = i - 1
-    @i
-    M=M-1
-    // goto LOOP
-    @LOOP
-    0;JMP
-    (CONT)
+  @20
+  D=A
+  @R1
+  M=D 
+  @99
+  D=A 
+  @20
+  M=D
+  @R1
+  A=M
+  D=M 
+  @R4
+  M=D
